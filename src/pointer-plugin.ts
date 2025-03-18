5da8e8cc684c1e51be9bac01e0cf1362fe80ad6b
pkpk <mqbdr314@gmail.com>
2025-03-18 10:27:24 +0900

    タッチを無視するように変更 タッチしたときに発火するトリガーを追加してタッチ操作されていることがわかるようにした

--- src/pointer-plugin.ts
+++ src/pointer-plugin.ts
@@ -14,7 +14,11 @@ export const PointerPlugin: PointerPluginStatic = class implements PointerPlugin
 	latestPointerPoint: Readonly<g.CommonOffset> | null;
 
 	_onMouseMove_bound: (e: MouseEvent) => void;
+	_onPointerMove_bound: (e: PointerEvent) => void;
 	_getScale: () => { x: number; y: number };
+
+	/** タッチを検出したときに発火 */
+	onTouch: g.Trigger;
 
 	static isSupported(): boolean {
 		return (typeof document !== "undefined") && (typeof document.addEventListener === "function");
@@ -28,20 +32,43 @@ export const PointerPlugin: PointerPluginStatic = class implements PointerPlugin
 		this._getScale = (viewInfo as any).getScale ? () => (viewInfo as any).getScale() : () => { return { x: 1, y: 1 } };
 
 		this._onMouseMove_bound = this._onMouseMove.bind(this);
+		this._onPointerMove_bound = this._onPointerMove.bind(this);
+
+		this.onTouch = new g.Trigger();
 	}
 
 	start(): boolean {
-		this.view.addEventListener("mousemove", this._onMouseMove_bound, false);
+		if("onpointermove" in (this.view as any)) {
+			this.view.addEventListener("pointermove", this._onPointerMove_bound, false);
+		} else {
+			this.view.addEventListener("mousemove", this._onMouseMove_bound, false);
+		}
 		return true;
 	}
 
 	stop(): void {
+		this.view.removeEventListener("pointermove", this._onPointerMove_bound, false);
 		this.view.removeEventListener("mousemove", this._onMouseMove_bound, false);
 	}
 
 	_onMouseMove(e: MouseEvent): void {
-		const scene = this.game.scene();
-		if (!scene) return;
+		const rect = this.view.getBoundingClientRect();
+		const positionX = rect.left + window.pageXOffset;
+		const positionY = rect.top + window.pageYOffset;
+		const offsetX = e.pageX - positionX;
+		const offsetY = e.pageY - positionY;
+		const scale = this._getScale();
+
+		const point = { x: offsetX / scale.x, y: offsetY / scale.y };
+		this.latestPointerPoint = point;
+	}
+
+	_onPointerMove(e: PointerEvent): void {
+		// タッチなら無視する
+		if(e.pointerType === "touch") {
+			this.onTouch.fire();
+			return;
+		}
 
 		const rect = this.view.getBoundingClientRect();
 		const positionX = rect.left + window.pageXOffset;
